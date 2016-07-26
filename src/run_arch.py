6c24237f1e64fde46229627b8bbffb1254443a74
acedarney <acedarney@gmail.com>
2016-07-25 21:46:39 -0400

    Add Unit Test

--- src/run_arch.py
+++ src/run_arch.py
@@ -5,10 +5,22 @@ class Foo:
     pass
 
 
-foo = Foo()
-names = ['a', 'b', 'c']
-for attrs in names:
-    setattr(foo, attrs, 10)
-foo.d = foo.a + foo.b + foo.c
+def create_foo():
+    foo = Foo()
+    names = ['a', 'b', 'c']
+    for attrs in names:
+        setattr(foo, attrs, 10)
+    foo.d = foo.a + foo.b + foo.c
+    return foo
 
-print(vars(foo))
+# print(vars(foo))
+
+
+def test_foo1():
+    foo = create_foo()
+    assert foo.a == 10
+
+
+def test_foo2():
+    foo = create_foo()
+    assert foo.b == 1
